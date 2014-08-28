0f0ec3d17facdc6188379c00b7f37ae8b0083408
gwsilva <gideoni.silva@omnes.net.br>
2014-08-28 16:05:41 -0300

    Create __openerp__.py

---
+++ rdc_16/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Gideoni Silva (Omnes)
+#    Copyright 2013-2014 Omnes Tecnologia
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Maximplant Custom',
+    'description': """
+  ==================================
+  Maximplant Processes Customization
+  ==================================
+    """,
+    'category': 'Inventory, Logistic'
+    'license': 'AGPL-3',
+    'author': 'Omnes Tecnolgia',
+    'website': 'http://www.omnes.net.br',
+    'version': '0.1',
+    'depends': ['stock','web_m2x_options','fill_move_line_by_origin'],
+    'data': ['stock_view.xml'],    
+    'demo': [],
+    'images': [],
+    'installable': True,
+    'active': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
