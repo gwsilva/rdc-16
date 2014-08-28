a8d51fc78414feb54430086f6b79a0940a7d436a
gwsilva <gideoni.silva@omnes.net.br>
2014-08-28 15:56:21 -0300

    Update __init__.py

--- rdc_16/__init__.py
+++ rdc_16/__init__.py
@@ -20,6 +20,6 @@
 #
 ##############################################################################
 
-import stock
+from . import stock
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
